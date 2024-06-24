80d3203a5efd61d2ee4363673cf01fa22fd5a666
ramazanerduran <ramazan.erduran@outlook.com.tr>
2024-06-24 19:07:27 +0300

    add apps color palette

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -10,6 +10,19 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+# APP COLOR PALETTE
+# --primary-100:#FF6600;
+# --primary-200:#ff983f;
+# --primary-300:#ffffa1;
+# --accent-100:#F5F5F5;
+# --accent-200:#929292;
+# --text-100:#FFFFFF;
+# --text-200:#e0e0e0;
+# --bg-100:#1D1F21;
+# --bg-200:#2c2e30;
+# --bg-300:#444648;
+    
+
 # Header
 st.markdown(
     """
